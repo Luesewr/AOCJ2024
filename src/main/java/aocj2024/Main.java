5ec0b851b5146799c7988c63a9e559bdf3686388
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2024-12-04 11:23:47 +0100

    Day 4

--- src/main/java/aocj2024/Main.java
+++ src/main/java/aocj2024/Main.java
@@ -2,7 +2,7 @@ package aocj2024;
 
 public class Main {
     public static void main(String[] args) {
-        Day day = new Day3();
+        Day day = new Day4();
         day.part1();
         day.part2();
     }
