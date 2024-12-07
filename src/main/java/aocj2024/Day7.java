1467d5c5ed5f2281021c80cfd3840273e3f4a1a7
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2024-12-07 12:39:19 +0100

    Day 7 OOP'ification

--- src/main/java/aocj2024/Day7.java
+++ src/main/java/aocj2024/Day7.java
@@ -2,79 +2,77 @@ package aocj2024;
 
 import java.util.Arrays;
 import java.util.List;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.function.TriFunction;
 
 public class Day7 extends Day {
     @Override
     public void part1() {
-        List<Pair<Long, long[]>> equations = getLinesFromFile("day7.txt")
-                .map(string -> string.split(": "))
-                .map(strings -> Pair.of(strings[0], strings[1]))
-                .map(pair -> Pair.of(Long.parseLong(pair.getLeft()), Arrays.stream(pair.getRight().split(" ")).mapToLong(Long::parseLong).toArray()))
-                .toList();
+        List<String> lines = getLinesFromFile("day7.txt").toList();
+        List<Equation> equations = Equation.parseEquations(lines);
 
-        long calibratedTotal = equations.stream()
-                .filter(equation -> evaluateOptions(equation.getLeft(), 0, equation.getRight()))
-                .map(Pair::getLeft)
-                .reduce(0L, Long::sum);
+        long calibratedTotal = Equation.sumCalibrated(equations, Equation::evaluateOptions);
 
         System.out.println(calibratedTotal);
     }
 
     @Override
     public void part2() {
-        List<Pair<Long, long[]>> equations = getLinesFromFile("day7.txt")
-                .map(string -> string.split(": "))
-                .map(strings -> Pair.of(strings[0], strings[1]))
-                .map(pair -> Pair.of(Long.parseLong(pair.getLeft()), Arrays.stream(pair.getRight().split(" ")).mapToLong(Long::parseLong).toArray()))
-                .toList();
+        List<String> lines = getLinesFromFile("day7.txt").toList();
+        List<Equation> equations = Equation.parseEquations(lines);
 
-        long calibratedTotal = equations.stream()
-                .filter(equation -> evaluateOptionsWithConcat(equation.getLeft(), 0, equation.getRight()))
-                .map(Pair::getLeft)
-                .reduce(0L, Long::sum);
+        long calibratedTotal = Equation.sumCalibrated(equations, Equation::evaluateOptionsWithConcat);
 
         System.out.println(calibratedTotal);
     }
 
-    private static boolean evaluateOptions(long goal, long current, long[] input) {
-        if (current > goal) return false;
-        if (current == goal && input.length == 0) return true;
-        if (input.length == 0) return false;
+    private record Equation(long[] values, long goal) {
+        private boolean evaluateOptions(long current, int offset) {
+            if (current > goal) return false;
+            if (current == goal && values.length == offset) return true;
+            if (offset >= values.length) return false;
 
-        long plusCurrent = current + input[0];
-        long[] plusRest = Arrays.copyOfRange(input, 1, input.length);
+            long plusCurrent = current + values[offset];
+            if (evaluateOptions(plusCurrent, offset + 1)) return true;
 
-        if (evaluateOptions(goal, plusCurrent, plusRest)) return true;
+            long multiplyCurrent = current * values[offset];
+            return evaluateOptions(multiplyCurrent, offset + 1);
+        }
 
-        long multiplyCurrent = current * input[0];
-        long [] multiplyRest = Arrays.copyOfRange(input, 1, input.length);
+        private boolean evaluateOptionsWithConcat(long current, int offset) {
+            if (current > goal) return false;
+            if (current == goal && values.length == offset) return true;
+            if (offset >= values.length) return false;
 
-        if (evaluateOptions(goal, multiplyCurrent, multiplyRest)) return true;
+            long plusCurrent = current + values[offset];
+            if (evaluateOptionsWithConcat(plusCurrent, offset + 1)) return true;
 
-        return false;
-    }
+            long multiplyCurrent = current * values[offset];
+            if (evaluateOptionsWithConcat(multiplyCurrent, offset + 1)) return true;
 
-    private static boolean evaluateOptionsWithConcat(long goal, long current, long[] input) {
-        if (current > goal) return false;
-        if (current == goal && input.length == 0) return true;
-        if (input.length == 0) return false;
+            long concatCurrent = Long.parseLong(Long.toString(current) + values[offset]);
+            return evaluateOptionsWithConcat(concatCurrent, offset + 1);
+        }
 
-        long plusCurrent = current + input[0];
-        long[] plusRest = Arrays.copyOfRange(input, 1, input.length);
+        private static long sumCalibrated(List<Equation> equations, TriFunction<Equation, Long, Integer, Boolean> calibrateFunction) {
+            return equations.stream()
+                    .filter(equation -> calibrateFunction.apply(equation, 0L, 0))
+                    .map(equation -> equation.goal)
+                    .reduce(0L, Long::sum);
+        }
 
-        if (evaluateOptionsWithConcat(goal, plusCurrent, plusRest)) return true;
+        private static List<Equation> parseEquations(List<String> lines) {
+            return lines.stream()
+                    .map(Equation::parseEquation)
+                    .toList();
+        }
 
-        long multiplyCurrent = current * input[0];
-        long[] multiplyRest = Arrays.copyOfRange(input, 1, input.length);
+        private static Equation parseEquation(String equationString) {
+            String[] parts = equationString.split(": ");
 
-        if (evaluateOptionsWithConcat(goal, multiplyCurrent, multiplyRest)) return true;
+            long goal = Long.parseLong(parts[0]);
+            long[] values = Arrays.stream(parts[1].split(" ")).mapToLong(Long::parseLong).toArray();
 
-        long concatCurrent = Long.parseLong(Long.toString(current) + input[0]);
-        long[] concatRest = Arrays.copyOfRange(input, 1, input.length);
-
-        if (evaluateOptionsWithConcat(goal, concatCurrent, concatRest)) return true;
-
-        return false;
+            return new Equation(values, goal);
+        }
     }
 }
