84b5c2faef4f9840bd42b64cd87bf74dd183f665
LuesewrBDO <laurens.de.swart@bdo.nl>
2024-12-10 14:56:31 +0100

    Day 9 technical correctness

--- src/main/java/aocj2024/Day09.java
+++ src/main/java/aocj2024/Day09.java
@@ -87,8 +87,9 @@ public class Day09 extends Day {
                 int size = Integer.parseInt(String.valueOf(line.charAt(i)));
 
                 if (i % 2 == 0) {
-                    for (int j = 0; j < size; j += maxChunkSize) {
-                        int chunkSize = Integer.min(size, maxChunkSize);
+                    int chunkSize = Integer.min(size, maxChunkSize);
+
+                    for (int j = 0; j < size; j += chunkSize) {
                         FileFragment fileFragment = new FileFragment(i / 2, startIndex + j, chunkSize);
                         fileFragments.add(fileFragment);
                     }
